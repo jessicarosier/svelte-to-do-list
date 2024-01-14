106e54ca9ceda8325d83a4d3dbb7708dd39fd8a1
Jessica Rosier <jessica.c.rosier1@gmail.com>
2024-01-14 13:34:29 -0600

    added dynamic heading

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,57 +1,71 @@
 <script>
 
-    import { slide } from 'svelte/transition';
+    import {slide} from "svelte/transition";
 
-  let toDoItems = [
-      {
-          name: "Study for exam.",
-          completed: true,
-      },
-      {
-          name: "Feed the dog.",
-          completed: false,
-      },
-      {
-          name: "Clean the cats litter box.",
-          completed: false,
-      },
-  ];
+    let toDoItems = [
+        {
+            name: "Study for exam.",
+            completed: true,
+        },
+        {
+            name: "Feed the dog.",
+            completed: false,
+        },
+        {
+            name: "Clean the cats litter box.",
+            completed: false,
+        },
+    ];
 
-  let name = "";
+    let name = "";
 
-  function addToDo() {
-      const newItem = {
-          name: name,
-          completed: false,
-      }
+    function addToDo() {
+        const newItem = {
+            name: name,
+            completed: false,
+        };
 
-      toDoItems = [...toDoItems, newItem]
-      name = "";
-  }
+        toDoItems = [...toDoItems, newItem];
+        name = "";
+    }
+
+    //when the delete button is clicked  it will remove the item from the array
+    function deleteToDo() {
+        toDoItems = toDoItems.filter(todo => !todo.completed);
+    }
+
 
 </script>
 
 
 <div>
-  <input type="text" bind:value={name} />
-  <button on:click={addToDo} >Add Todo</button>
+  <input type="text" bind:value={name}/>
+  <button on:click={addToDo}>Add Todo</button>
+
 </div>
 
+
 <div>
-  <ul>
-    {#each toDoItems.filter(todo => todo.completed) as todo}
-      <li transition:slide>
-        {todo.name}
-        <input type="checkbox" bind:checked={todo.completed} />
-      </li>
-      {/each}
-  </ul>
+
+  <h2>{toDoItems.filter(todo => !todo.completed).length} items of {toDoItems.length} left to do.</h2>
+
 
   <ul>
     {#each toDoItems.filter(todo => !todo.completed) as todo}
       <li transition:slide>
+        <input type="checkbox" bind:checked={todo.completed}/>
         {todo.name}
-        <input type="checkbox" bind:checked={todo.completed} />
+        <button>Delete</button>
+      </li>
+    {/each}
+  </ul>
+
+  <ul>
+    {#each toDoItems.filter(todo => todo.completed) as todo}
+      <li transition:slide>
+        <input type="checkbox" bind:checked={todo.completed}/>
+        {todo.name}
+        <button on:click={deleteToDo}>Delete</button>
       </li>
     {/each}
   </ul>
@@ -60,10 +74,19 @@
 
 <style>
 
-  div {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-  }
+    div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    h2 {
+        text-align: center;
+    }
+
+    ul {
+        list-style: none;
+    }
 
 </style>
